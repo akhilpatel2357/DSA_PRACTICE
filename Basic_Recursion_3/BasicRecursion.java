07f6ee90698cf3e3e9396cea7f14f936c344990a
Akhil <akhilpatel2357@gmail.com>
2025-10-26 18:03:07 +0530

    Recursion commits

--- Basic_Recursion_3/BasicRecursion.java
+++ Basic_Recursion_3/BasicRecursion.java
@@ -110,17 +110,115 @@ public class BasicRecursion {
 		}
 		return true;
 	}
-	public static void main(String[] args) {
-		String a = "eye ,eye";
-		System.out.println(checkSPalindrome(a));
+	
+	//TC: O(n), SC: O(1)
+	public static boolean checkStringPalin(String s) {
+		  int l = 0;
+			int h = s.length() -1;
+			
+			while(l <= h) {
+	            char first = s.charAt(l);
+	            char last = s.charAt(h);
+				if(!Character.isLetterOrDigit(first)) {
+					l++;
+				} else if(!Character.isLetterOrDigit(last)) {
+					h--;
+				} else if(Character.toLowerCase(first) != Character.toLowerCase(last)) {
+					return false;
+				} else {
+					l++;
+					h--;
+				}
+			}
 		
-//		int[] org = new int[] {1,2,3,4,5, 6,7, 8,9,10};
-//		int[] rev = new int[org.length];
-//	//	reverseArr(org, rev, 0, org.length-1);
-//		
-//		reverseArray(org, 0, org.length -1);
-//		for(int i: org) {
-//			System.out.print(i + " ");
-//		}
+			return true;
 	}
+	
+	//TC: O(n), SC: O(n) ignore recursion stack means Sc: O(1)
+	public static boolean checkPalinRecur(String s, int l, int h) {
+		
+		boolean result = true;
+		if(l <= h) {
+            char first = s.charAt(l);
+            char last = s.charAt(h);
+			if(!Character.isLetterOrDigit(first)) {
+				l++;
+			} else if(!Character.isLetterOrDigit(last)) {
+				h--;
+			} else if(Character.toLowerCase(first) != Character.toLowerCase(last)) {
+				return false;
+			} else {
+				l++;
+				h--;
+			}
+			result = checkPalinRecur(s,l,h);
+		}
+		return result;
+	}
+	
+	//TC: O(2^n), SC: O(n)
+	public static int f(int i) {
+		if(i == 0) 
+			return 0;
+		if(i == 1)
+			return 1;
+		return f(i-2) +f(i-1);
+		
+	}
+	
+	//TC: O(n), SC: O(1)
+	public static void fibonacci(int n) {
+        int start = 0;
+        int nextStart = 1;
+        int temp = 0;
+    	System.out.print(temp + " ");
+        for(int i = 1; i <=n; i++) {
+        	System.out.print(nextStart+" ");
+
+        	temp = start+nextStart;
+        	start = nextStart;
+        	nextStart = temp;
+        }
+        
+	}
+
+	//TC: O(n)+O(n) for calculating and printing, SC: O(n)
+	public static void fibUsingArr(int n)  {
+		int[] arr = new int[n+1];
+		
+		 arr[0] = 0;
+		 arr[1] = 1;
+		 
+		 for(int i = 2; i <= n; i++) {
+			 arr[i] = arr[i-2] +arr[i-1];
+		 }
+		 for(int i : arr) {
+			 System.out.print(i + " ");
+		 }
+		
+	}
+		
+	
+	//TC: O(n), SC: O(1)
+	public static void fibnocciSeriesDekh(int n) {
+		int start = 0;
+		int nextStart = 1;
+		if(n == 0) {
+			System.out.print(start + " ");
+			return;
+		}
+		System.out.print(start+" "+nextStart+" ");
+		for(int i = 2; i <=n; i++) {
+			int current = start + nextStart;
+			start = nextStart;
+			nextStart= current;
+			System.out.print(current+" ");
+		}
+	}
+	public static void main(String[] args) {
+		
+		
+		int n = 6;
+		fibnocciSeriesDekh(n);
+}
 }
