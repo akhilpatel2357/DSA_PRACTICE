e160e8474d08c52a4d896fa68e137d267c5c23c5
Akhil <akhilpatel2357@gmail.com>
2025-10-28 20:05:13 +0530

    Hashing string

--- Basic_Hashing_4/Hashing.java
+++ Basic_Hashing_4/Hashing.java
@@ -1,9 +1,8 @@
 package Basic_Hashing_4;
 import java.util.*;
-
-import java.util.ArrayList;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 public class Hashing {
 	
 	//TC: O(n*n), SC: O(n)
@@ -42,6 +41,7 @@ System.out.println(maps);
 	
 	//TC: O(n), SC: O(n)
 public static void countFreqElements(int[] arr){
+	
 		
 		Map<Integer, Integer> m= new HashMap<>();
 		for(int i =0; i<arr.length;i++) {
@@ -69,8 +69,77 @@ public static void countFreqElements(int[] arr){
 
 		
 	}
+
+//TC:O(n), SC:O(1)
+public static int[] charHashLowerCase(String s) {
+	int[] arr = new int[26];
+	for(int i = 0; i <s.length(); i++) {
+		arr[s.charAt(i) - 'a'] = arr[s.charAt(i) -'a']+1;
+	}
+	return arr;
+}
+//TC:O(n), SC:O(1)
+public static  int[] charHash(String s) {
+	
+	
+	int[] arr = new int[255];
+	
+	for(int i = 0; i <s.length();i++){
+		arr[s.charAt(i)] = arr[s.charAt(i)]+1;
+	}
+	return arr;
+}
+
+
+//TC:O(n), SC:O(1)
+public static int particularCharHash(String s, char character) {
+	
+	int count = 0;
+	for(int i =0; i <s.length();i++) {
+		if(character == s.charAt(i)) {
+			++count;
+		}
+	}
+	
+	return count;
+}
+
+
+public static Map<Character, Integer> countStringUsingHashMap(String s) {
+	Map<Character, Integer> map = new HashMap<Character, Integer>();
+	
+	for(int i = 0; i < s.length(); i++) {
+		map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+	}
+	return map;
+}
+
+//TC: O(n), SC: O(n)
+public static Map<String, Long> charHasingUsingStreaming(String s) {
+  Map<String, Long> map = Stream.of(s.split("")).collect(Collectors.groupingBy(s1->s1, Collectors.counting()));
+	return map;
+}
+
 public static void main(String[] args) {
-	countFreqElements(new int[] {1,1,2,2,2,2,2,2,3,4,5,56,7,7,88,9,9,10000000});
+	
+	System.out.println(charHasingUsingStreaming("abcABCABCabczzzz"));
+	
+//	Map<Character, Integer> map = countStringUsingHashMap("abcABCABCabczzzz");
+	
+//	for(Entry<Character, Integer> entry : map.entrySet()) {
+//		if(entry.getValue() != 0) {
+//			System.out.println(entry.getKey()+">>>>>>"+entry.getValue());
+//		}
+//	}
+//	
+	
+//System.out.println(particularCharHash("abcabcabczABCABCABCaaaaa    123",'a'));
+//
+//int[] arr = charHashLowerCase("abcabcabc");
+//for(int i = 0; i <arr.length;i++) {
+//	if(arr[i] != 0) 
+//	System.out.println((char)(i+'a')+">>>" +arr[i]);
+//}
 
 	
 }
